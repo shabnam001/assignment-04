bf320facd9bc9faf27da7045e6edfba175a838e1
Mac <mazharulislame.cse@gmail.com>
2023-08-10 13:00:16 +0600

    assignment5 done

--- problem-no-05.js
+++ problem-no-05.js
@@ -1,1 +1,20 @@
-f
+function canPay(changeArray, totalDue) {
+    if(changeArray[0] == null){
+        return 'please provide an element'
+    }
+    let sum = 0;
+    for(let i = 0; i < changeArray.length; i++){
+        const index = i;
+        const element = changeArray[index];
+        sum = sum + element;
+    }
+    if(sum >= totalDue){
+        return true;
+    }
+    if(sum < totalDue){
+        return false;
+    }
+    
+}
+let arr = [5, 5, 5];
+console.log(canPay(arr, 10));
